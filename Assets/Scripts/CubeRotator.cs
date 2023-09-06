9d92edcc22a1d6c30ea2dc882cb3ba6e3323d3fe
NikoNI776 <niko57gui@gmail.com>
2023-09-06 10:23:05 +0100

    Apenas um Teste
    Coloquei um cubo Rodando na cena "SimpleScene" para testar se está funcionando a conexão dos arquivos

---
+++ Assets/Scripts/CubeRotator.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotator : MonoBehaviour
+{
+    public float RotationSpeed = 30f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * RotationSpeed * Time.deltaTime);
+    }
+}
