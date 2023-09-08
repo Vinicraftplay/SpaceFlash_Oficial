308647889bc231f4d90d88421149a722da662bf5
Emiclys <emiclys1@gmail.com>
2023-09-07 22:56:17 -0300

    Controle do player
    Agora ele anda e vira a câmera.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -8,6 +8,7 @@ public class PlayerControl : MonoBehaviour
     Transform camTransform;
 
     public float sensitivity = 0.1f;
+    public float speed = 4f;
 
     Vector2 camRotation;
 
@@ -21,6 +22,7 @@ public class PlayerControl : MonoBehaviour
 
     void Update()
     {
+        // Movimentação do jogador
         if (Keyboard.current.wKey.wasPressedThisFrame) walkVector.z++;
         if (Keyboard.current.wKey.wasReleasedThisFrame) walkVector.z--;
         if (Keyboard.current.sKey.wasPressedThisFrame) walkVector.z--;
@@ -31,14 +33,14 @@ public class PlayerControl : MonoBehaviour
         if (Keyboard.current.dKey.wasPressedThisFrame) walkVector.x++;
         if (Keyboard.current.dKey.wasReleasedThisFrame) walkVector.x--;
 
-        realWalkVector = Vector3.LerpUnclamped(realWalkVector, walkVector, 10 * Time.deltaTime);
+        realWalkVector = Vector3.LerpUnclamped(realWalkVector, walkVector * speed, 10 * Time.deltaTime);
+
+        transform.Translate(realWalkVector * Time.deltaTime);
 
         // Mouselook
         camRotation = Mouse.current.delta.value * sensitivity;
-    }
 
-    void LateUpdate()
-    {
-        transform.Translate(realWalkVector);
+        transform.Rotate(0, camRotation.x, 0);
+        camTransform.Rotate(-camRotation.y, 0, 0);
     }
 }
